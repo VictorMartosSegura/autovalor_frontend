441d047e58caab2ec7730ee6d7577bc6f2c375d5
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-05 14:33:31 +0200

    Add placeholder sell car page

---
+++ src/views/SellCarPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Vender coche</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h2>Crear anuncio</h2>
+      <p>Esta pantalla se conectara con el backend para crear anuncios, subir imagenes y usar IA.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
