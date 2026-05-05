1f6a34f6a36ba4cab319726f4751b90bc62cb280
VictorMartosSegura <victormartossegura@elpuig.xeill.net>
2026-05-05 14:36:03 +0200

    Add placeholder admin dashboard page

---
+++ src/views/AdminDashboardPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Admin</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h2>Panel de administracion</h2>
+      <p>Esta pantalla se conectara con /api/admin para usuarios, anuncios, mensajes y estadisticas.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
